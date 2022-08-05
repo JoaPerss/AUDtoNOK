0725523a6d2ebce5183344dbd83f01c07d0191f9
JoaPerss <joachimlp@hotmail.no>
2022-08-05 13:56:12 +1000

    darkmodedefault

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,18 @@ import Head from 'next/head'
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export default function Home() {
 
   const callAPI = async () => {
-
     function getInput() {
       let numberInput = document.getElementById("input").value;
 
@@ -29,31 +36,10 @@ export default function Home() {
     } catch (err) {
       console.log(err);
     }
-
-    /* var myHeaders = new Headers();
-    myHeaders.append("apikey", process.env.API_URL);
-
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-      headers: myHeaders
-    };
-
-    try{  
-    await fetch(
-      fetch("https://api.apilayer.com/exchangerates_data/convert?to=NOK&from=AUD&amount=" +getInput(), requestOptions)
-        .then(response => response.json())
-        .then((result) => {
-          console.log(result.result);
-          document.getElementById("finalRes").innerText = result.result.toFixed(2) + "NOK";
-        })
-        .catch(error => console.log('error', error)))
-        
-    }catch (err){
-      console.log(err);
-  } */
   }
   return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
     <div className="container">
       <Head>
         <title>Converter</title>
@@ -72,5 +58,6 @@ export default function Home() {
       </main>
 
     </div>
+    </ThemeProvider>
   )
 }
